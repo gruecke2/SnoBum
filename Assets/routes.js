90571021d8d16fd212e2d2c70f1052e1c8cbb4ed
GarretR <garret.rueckert.guitar@gmail.com>
2018-12-19 19:06:14 -0700

    Some API calls working, location finder working

---
+++ Assets/routes.js
@@ -0,0 +1,8 @@
+/**
+ * routes.js will format the API request to Google Maps
+ * for each resort when clicked on. Defaults to a bus ride.
+ * 
+ * 
+ */
+
+ 
--- garret.js
+++
@@ -1,1 +0,0 @@
-console.log("We r kewl.");
