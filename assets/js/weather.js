1439e634b7644cb8cbb2de20212a81f8a5c993d4
GarretR <garret.rueckert.guitar@gmail.com>
2019-01-05 01:01:46 -0700

    Just needs readme

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -17,7 +17,7 @@ function getWeather(search){
 
       console.log(response);
   
-        $("#weather-head").html("<h4>Weather Information for " + response.name + " </h4>");
+        // $("#weather-head").html("<h4>Weather Information for " + response.name + " </h4>");
         $("#description").html("<strong>Current Conditions:</strong> " + response.weather[0].main +
          " " + "<img src='http://openweathermap.org/img/w/" + response.weather[0].icon +".png'>");
 
@@ -30,26 +30,37 @@ function getWeather(search){
 
 $("#alta").on("click", function(){
   getWeather("lat=40.5888&lon=-111.6552");
+  $("#weather-head").html("<h4>Weather Information for " + "Alta </h4>");
 })
 
 $("#snowbird").on("click", function(){
   getWeather("lat=40.5819&lon=-111.6552");
+  $("#weather-head").html("<h4>Weather Information for " + "Snowbird </h4>");
+
 })
 
 $("#brighton").on("click", function(){
   getWeather("lat=40.5980&lon=-111.5832");
+  $("#weather-head").html("<h4>Weather Information for " + "Brighton </h4>");
+
 })
 
 $("#park-city").on("click", function(){
   getWeather("lat=40.6461&lon=-111.4980");
+  $("#weather-head").html("<h4>Weather Information for " + "Park City </h4>");
+
 })
 
 $("#deer-valley").on("click", function(){
   getWeather("lat=40.6374&lon=-111.4783");
+  $("#weather-head").html("<h4>Weather Information for " + "Deer Valley </h4>");
+
 })
 
 $("#solitude").on("click", function(){
   getWeather("lat=40.6211&lon=-111.5933");
+  $("#weather-head").html("<h4>Weather Information for " + "Solitude </h4>");
+
 })
 
 //#endregion
